96bf76b94f72831f2002f48c9ff5669924cb1dbf
BlackRabbit12 <bsblack12@gmail.com>
2020-02-17 13:44:54 -0800

    created base files

---
+++ model/validation.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ *
+ * @author Bridget Black
+ * @author Chad Drennan
+ * @version 1.0
+ * 2020-02-17
+ * Last Updated: 2020-02-17
+ * File Name: validation.php
+ * File Associations:
+ */

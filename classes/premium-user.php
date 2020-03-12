abb97649d0d8161a8905fb0fdb34a8907aded8f1
BlackRabbit12 <bsblack12@gmail.com>
2020-03-12 09:15:19 -0700

    registration form tests

--- classes/premium-user.php
+++ classes/premium-user.php
@@ -8,17 +8,24 @@
  * 2020-03-11
  * Last Updated: 2020-03-11
  */
-
 class PremiumUser extends User
 {
     //in addition, premium users have:
     private $_userName;
 
+    /**
+     * Get the user's username.
+     * @return mixed
+     */
     function getUserName()
     {
         return $this->_userName;
     }
 
+    /**
+     * Reset the user's username.
+     * @param $newUserName
+     */
     function setResetUserName($newUserName)
     {
         $this->_userName = $newUserName;

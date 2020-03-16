32bbe6acf99e6ed85c46aa012cdb3a7e2f7547d7
BlackRabbit12 <bsblack12@gmail.com>
2020-03-16 09:42:59 -0700

    edited login and registration

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
 
-require_once('/home/cdrennan/config-workout.php');
-//require_once('/home/bblackgr/config-workout.php');
+//require_once('/home/cdrennan/config-workout.php');
+require_once('/home/bblackgr/config-workout.php');
 
 /**
  * Database class interactions with database and workout tracker. Handles all database queries.

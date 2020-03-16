266056d5535d094ea34d0764aa906ed1567b4ce7
BlackRabbit12 <bsblack12@gmail.com>
2020-03-15 17:12:13 -0700

    javascript validation complete

---
+++ js/login_validation.js
@@ -0,0 +1,38 @@
+/*
+ * Client side validation with Javascript for Login and part of Register
+ *
+ * @author Bridget Black
+ * @author Chad Drennan
+ * @version 1.0
+ * 2020-03-15
+ */
+
+document.getElementById('formatted-login').onsubmit = validateLogin;
+
+function validateLogin() {
+    //user name
+    let user = document.getElementById('user-name').value;
+    user = user.trim();
+    if (user === "") {
+        document.getElementById('user-name').style.borderColor = "red";
+        return false;
+    }
+    if (!(/^[0-9a-zA-Z]+$/.test(user))) {
+        document.getElementById('user-name').style.borderColor = "red";
+        return false;
+    }
+
+    //password
+    let password = document.getElementById('password').value;
+    password = password.trim();
+    if (password === "") {
+        document.getElementById('password').style.borderColor = "red";
+        return false;
+    }
+    if (!(/^[0-9a-zA-Z]+$/.test(password))) {
+        document.getElementById('password').style.borderColor = "red";
+        return false;
+    }
+
+    return true;
+}
